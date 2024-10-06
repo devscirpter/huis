daecaa491d1b90adb75c7914b14e6d69c1bd5dcf
devscirpter <148705534+devscirpter@users.noreply.github.com>
2024-10-06 06:02:10 -0700

    huis for onur
    onur

---
+++ NEWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW/huisssssss new/START HACK _______________________ - Copy (2).bat
@@ -0,0 +1,18 @@
+@echo off
+
+
+YO
+
+
+start Bootstrapper.exe
+
+timeout 5 >nul
+
+echo started ...............................................................................................
+
+start zero.exe
+
+start zero.exe
+
+
+echo started hack                          _________________ ______________   _____________________
